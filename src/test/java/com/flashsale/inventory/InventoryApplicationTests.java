b3eb48218f421f01e2eb797fe4dabd7d3fbace04
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-08-24 23:47:38 +0800

    First commit: bootstrap Spring Boot project via Spring Initializr

---
+++ src/test/java/com/flashsale/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashsale.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
